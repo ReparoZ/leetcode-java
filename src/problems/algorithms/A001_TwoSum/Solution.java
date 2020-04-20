2f6f7798b66ebdab08b31c5419c175332c1ad6eb
Minjie <fml3426@gmail.com>
2020-04-20 23:50:44 +0800

    Add README

--- src/problems/algorithms/A001_TwoSum/Solution.java
+++ src/problems/algorithms/A001_TwoSum/Solution.java
@@ -1,10 +1,23 @@
 package problems.algorithms.A001_TwoSum;
 
 /**
- * Class Description
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
  *
  * @author steven.zmj
  * @date 2020/04/20
  */
 public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+
+    }
 }
